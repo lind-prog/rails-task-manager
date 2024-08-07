ff9e7ce35669f8632467b355b34e563449a20cc1
Linda Badji <linda.badji33@gmail.com>
2024-08-07 11:20:42 +0200

    raills

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_08_06_102823) do
+ActiveRecord::Schema[7.1].define(version: 2024_08_06_104357) do
   create_table "tasks", force: :cascade do |t|
     t.string "title"
     t.text "details"
-    t.boolean "completed"
+    t.boolean "completed", default: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
-
 end
